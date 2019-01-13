11f25f73878fa57088dc651c029746e3b544e751
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-01-13 15:12:27 -0700

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -11,27 +11,26 @@ import subprocess #module for making calls to system (ex. ifconfig)
 
 def change_mac(interface, newmac):
     print("[+] Changing MAC address for " + interface + " to " + newmac)
-	subprocess.call(["ifconfig", interface, "down"])
-	subprocess.call(["ifconfig", interface, "hw", "ether", newmac)
-	subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", newmac)
+    subprocess.call(["ifconfig", interface, "up"])
 	
 def display_interfaces(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-	#regular expression searches pattern of MAC address
-	
-	if mac_address_search_result:
-	    return mac_address_search_result.group(0)
-	else:
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    #regular expression that searches for pattern of MAC address; aa:11:a1:f1:ee:11, etc
+ 
+    if mac_address_search_result: #if this regex returns a result:
+        return mac_address_search_result.group(0) #print it
+	else: #otherwise there was an error with the search result
 	    print("[-] Not able to locate MAC address for specified interface.")
 		
 def get_arguments():
     parser = optparse.OptionParser() #create parser object variable
-	parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address of.")
-	parser.add_option("-m", "--mac", dest="newmac", help="MAC address to change to.")
-	(options, arguments) = parser.parse_args() #parses user input using parse_args, called from optparse, and stores them in two variables:
-	#options and arguments
-	
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address of.")
+    parser.add_option("-m", "--mac", dest="newmac", help="MAC address to change to.")
+    (options, arguments) = parser.parse_args() #parses user input using parse_args, called from optparse, and stores them in two variables:
+    #options and arguments
 	if not options.interface:
 	    parser.error("[-] Please specify an interface, use --help for more info.")
 	elif not options.newmac:
