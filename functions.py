3ab6851a4ca6bb37f641bc128af6859ab08cb0b5
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-02-02 16:56:45 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
-
+#!/usr/env/python3
 def print_name(name):
+    print("Name passed to function is", name)
+    
+#when this function is called, you will pass an argument to it which it will accept as the name variable:
+
+print_name("Name goes here")
+
+#can also pass in a variable predefined:
+name = "This is a predefined variable name"
+print_name(name)
