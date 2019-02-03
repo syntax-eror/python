e48071f3de42fb33e85807b574c20b100b09d06e
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-02-02 18:41:06 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@
 #define functions using the format:
 #def function_name(variable_to_pass_in):
     #indent code blocks here
+    
+#importing functions:
+#to import specific module from function
+from time import localtime, mktime, sleep, strftime
+#import module sleep from time
 
 def print_name(name):
     print("Name passed to function is", name)
