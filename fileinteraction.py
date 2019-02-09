dcc4ff919e2a3695d15810eb697c538a6d030a4a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-02-08 20:10:25 -0700

    Update fileinteraction.py

--- fileinteraction.py
+++ fileinteraction.py
@@ -18,3 +18,24 @@ print(opnfile.read())
 opnfile.seek(0)
 
 for lines in opnfile:
+    print(lines, end="")  #end="" specifies not to print new line after each object
+    
+    
+opnfile.close() #close file once finished
+opnfile.name
+
+#to create a copy of a file
+opnfilebase = open('opnfile.txt', 'r') #open the original file first and assign to name like file_base
+
+#create a new file which will be written to
+newopnfile = open('newopnfile.txt', 'w') #w - truncates any existing file, contents will be overwritten
+newopnfile.write(opnfilebase.read()) #takes opnfile.txt and reads it into new object newopnfile, then returns number of chars
+newopnfile.close() #close out the open file
+
+newopnfile = open(newopnfile.name, 'r+') #r+ lets read and write to new file
+newopnfile.read()
+newopnfile.seek(0)
+
+newopnfile.write("Cigarette Smoking Man\n") #will write to seek position, ovewriting what's already there by number of characters passed in
+newopnfile.seek(0)
+newopnfile.read()
