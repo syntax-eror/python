4ac1870c1d90b7f67be8f6cf36408ff77942653e
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-02-14 20:51:23 -0700

    Update macchanger-python3.py

--- macchanger-python3.py
+++ macchanger-python3.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+
+#MAC Address Changer from Learn Python and Ethical Hacking from Scratch course
+#by Zaid Al-Quraishi
+
+import optparse #module for parsing options passed in from command line
+import re #regular expression operator module
+import subprocess #module for making calls to system commands
+
+def change_mac(interface, newmac):
+    print("[+] Changing MAC address for", interface, "to", newmac)
+    subprocess.run(["ifconfig", interface])
 
