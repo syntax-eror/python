068533039ba960c0954ae0bd26c1ef98f9ed49da
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-03-19 16:28:51 -0700

    Update networkscanner-python3.py

--- networkscanner-python3.py
+++ networkscanner-python3.py
@@ -2,22 +2,40 @@
 
 import scapy.all as scapy
 
-def scan(ip):
-    arp_request = scapy.ARP(pdst = ip)
-    arp_request.show() #module in scapy to show detail of packet
+#def scan(ip):
+    #arp_request = scapy.ARP(pdst = ip) #pdst is a field that needs to be set to tell where the-
+    #ARP packet should be directed
+    #the specific field can be found using scapy.ls; scapy.ls(scapy.ARP())
+    #arp_request.show() #module in scapy to show detail of packet
     #arp_request.pdst = ip - another way to set the pdst
     #print(arp_request.summary())
-    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") #create Ethernet object called Broadcast
+    #broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") #create Ethernet object called Broadcast
     #that has the dst field set to broadcast address
-    broadcast.show()
-    print(broadcast.sumary())
+    #broadcast.show()
+    #print(broadcast.sumary())
     #scapy.ls(scapy.ARP()) #scapy module to return list of fields use in scapy.ARP
     #using this, you see that pdst is the field you want to set
     #scapy.arping(ip)
     #scapy.ls(scapy.Ether())
-    arp_request_broadcast = broadcast/arp_request #/ is a scapy method to append;
+    #arp_request_broadcast = broadcast/arp_request #/ is a scapy method to append;
     #this appends arp_request to the end of broadcast to create a new variable
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    #print(arp_request_broadcast.summary())
+    #arp_request_broadcast.show()
+    #answered, unanswered = scapy.srp(arp_request_broadcast, timeout = 1) - scapy.srp returns a couple of two lists
+    #-this stores them in these two variables
+    #-timeout used to exit if no response is received, otherwise it will continue running
+    print(answered.summary()) #summary is a scapy method that will list the info stored in answered variable
+    print(unanswered.summary())
     
-scan("10.0.2.1")
+#scan("10.0.2.1")
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout = 1)
+    print(answered.summary())
+    print(unanswered.summary())
+
+scan("10.0.2.1/24")
+    
