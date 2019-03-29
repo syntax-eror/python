1c0d10471c2a92916b545aa793bbb7dcd05019b4
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-03-29 12:42:54 -0700

    Update networkscanner-python3.py

--- networkscanner-python3.py
+++ networkscanner-python3.py
@@ -31,7 +31,7 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
-    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff")
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     #answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1)
     answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0] #only return element 0 from list;
