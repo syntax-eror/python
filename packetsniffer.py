72f2246c386839ea161a466e33cf94c5ac7e99d7
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-05-05 20:02:57 -0700

    Update packetsniffer.py

--- packetsniffer.py
+++ packetsniffer.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
-import scapy.all
+#Python 2 version of packet sniffer from LEHAPFS course
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet.show) #packet.show will give field of packet
+
+sniff("eth0")
