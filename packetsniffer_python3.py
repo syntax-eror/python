6a6a931e524aaf18854d854f01b011987ccd7359
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-05-05 18:46:57 -0700

    Update packetsniffer_python3.py

--- packetsniffer_python3.py
+++ packetsniffer_python3.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import scapy.all as scapy
+from scapy_http import http #need scapy_http installed
+#pip install scapy_http
+#pip3 install scapy_http
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="port 21")
+    #, filter=    filters can be tcp, udp, etc. or specify port by doing port 80
     
 def process_sniffed_packet(packet):
-    print(packet)
-
-    sniff("eth0")
+    if packet.haslayer(http.HTTPRequest): #haslayer - scapy method
+        print(packet)
     
+sniff("eth0")
