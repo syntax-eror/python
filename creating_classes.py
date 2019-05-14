9cd3b6444b84dd02747ba4d3e8469fa3a700bfa6
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-05-14 15:27:37 -0700

    Update creating_classes.py

--- creating_classes.py
+++ creating_classes.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
+# simple class as example
+
+class Bike:
+    """"
+    This is a docstring which describes the class
+    This class Bike models a bike with engine and tires
+    """
+    
+    def __init__(self): #methods are functions that are inside classes
+        #__ double under - dunder
+        """
+        Docstring describing the method
+        """
+        self.engine = engine
+        self.tires = tires
+        #define the data that will make up the class
+        #pass - use pass if you do not implement method currently
+    
+    
+    
+        
 
