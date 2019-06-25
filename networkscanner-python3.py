f5028a062bdba15dfedf0c64c3f59ae7c8917b20
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-06-25 16:00:31 -0700

    Update networkscanner-python3.py

--- networkscanner-python3.py
+++ networkscanner-python3.py
@@ -6,8 +6,8 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    #append scapy layer "arp_request" to scapy layer "broadcast
-    #this is a scapy function
+    #/ is a scapy method to append arp_request, a scapy layer, to broadcast, another scapy layer
+    #  this creates a packet with an Ethernet layer (dst = broadcast) and an ARP layer (op = who-has)
     
     #answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1)
     answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0] #only return element 0 from list;
@@ -31,7 +31,7 @@ def scan(ip):
     return clients_list
 
 def print_result(results_list):
-    print("IP\t\t\tMAC Address\n==========================================")
+    print("\nIP\t\t\tMAC Address\n==========================================")
     for client in results_list:
         print(client["ip"], "\t\t", client["mac"])
 
