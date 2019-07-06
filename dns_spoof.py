df22ca12d678ad4b5253bb06aa2f8b0f0fd996f0
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-06 16:15:14 -0700

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 #iptables -I FORWARD -j NFQUEUE --queue-num 0
+#iptables --flush - delete rules set up
 
 import netfilterqueue
 #need to have module installed - pip3 install netfilterqueue
@@ -8,6 +9,8 @@ import netfilterqueue
 
 def process_packet(packet):
     print(packet)
+    packet.drop() #drop packets, connectivity cut
+    packet.accept() #forward packets to target, connectivity seems normal
 
 
 queue = netfilterqueue.NetfilterQueue()
@@ -15,3 +18,8 @@ queue.bind(0, process_packet) #invoke bind method of NetfilterQueue
 #0 - queue number used in iptables
 #process packet is a callback function that will be run on each packet
 queue.run
+
+#all packets received will be put in the queue that was set up
+#on target computer, connectiivty will be interrupted as each packet is being held
+
+
