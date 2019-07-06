421704f2613ed35741b92993d0fd1227b06d4724
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-06 14:54:46 -0700

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+#iptables -I FORWARD -j NFQUEUE --queue-num 0
+
 import netfilterqueue
+#need to have module installed - pip3 install netfilterqueue
+#apt-get install build-essential python-dev libnetfilter-queue-dev if errors
+
+def process_packet(packet):
+    print(packet)
+
 
 queue = netfilterqueue.NetfilterQueue()
-queue.bind() #invoke bind method of NetfilterQueue
+queue.bind(0, process_packet) #invoke bind method of NetfilterQueue
+#0 - queue number used in iptables
+#process packet is a callback function that will be run on each packet
+queue.run
