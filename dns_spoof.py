698e4c89480832c3118bfbef30579770a70aa252
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-06 20:16:50 -0700

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -2,13 +2,21 @@
 
 #iptables -I FORWARD -j NFQUEUE --queue-num 0
 #iptables --flush - delete rules set up
+#iptables -I OUTPUT -j NFQUEUE --queue-num 0
+#set up for your own computer
+
 
 import netfilterqueue
 #need to have module installed - pip3 install netfilterqueue
 #apt-get install build-essential python-dev libnetfilter-queue-dev if errors
 
 def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload()) #store packet as a variable;
+    #wrapped in scapy layer that lets you access layers of packet using scapy;
+    #then able to modify
     print(packet)
+    print(packet.getpayload()) #shows raw output of packet
+    print(scapy_packet.show()) #using new scapy created var, show fields of packet
     packet.drop() #drop packets, connectivity cut
     packet.accept() #forward packets to target, connectivity seems normal
 
