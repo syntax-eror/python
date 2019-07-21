5c6069dbe34841392348c65537d34f936f473d5a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-20 19:53:33 -0700

    Update arpspoof_detector_python3.py

--- arpspoof_detector_python3.py
+++ arpspoof_detector_python3.py
@@ -5,18 +5,12 @@
 #Need to run as python 2, http layer support is different now that scapy_http is deprecated
 
 import scapy.all as scapy
-from scapy.layers import http #need scapy_http installed
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
     
 def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest): #haslayer - scapy method        
-        url = get_url(packet)
-        print("[+] HTTP Request: " + url)
-        
-        login_info = get_login_info(packet)
-        if login_info:
-            print("\n\n[+] Possible username/password found:" + login_info + "\n\n")
-            
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+        print(packet.show())        
+    
 sniff("eth0")
