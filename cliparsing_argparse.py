c9490d6294221b8ff0280953969fd43744d92f27
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-11-22 16:42:32 -0700

    Update cliparsing_argparse.py

--- cliparsing_argparse.py
+++ cliparsing_argparse.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse #argparse standard library for parsing command line arguments
+# documentation for argparse:
+# https://docs.python.org/3/library/argparse.html?highlight=argparse#module-argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='the file to read')
