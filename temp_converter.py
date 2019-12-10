a2457f47e7a34856ef6c62eaf6bd4aecfb187d63
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-12-10 14:10:49 -0700

    Update temp_converter.py

--- temp_converter.py
+++ temp_converter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-#C to F = T(°C) × 1.8 + 32
 #F to C = (T(°F) - 32) / 1.8
+#C to F = T(°C) × 1.8 + 32
 
 def convert_to_celcius(ftemp):
     return (ftemp - 32) / 1.8
@@ -9,11 +9,11 @@ def convert_to_celcius(ftemp):
 def convert_to_fahrenheit(ctemp):
     return (ctemp * 1.8) + 32
 
-ctemp = input("Enter temperature to convert in Celsius:")
-ftemp = input("Enter temperature to convert in Fahrenheit:")
+ctemp = input("Enter temperature to convert in Celsius: ")
+ftemp = input("Enter temperature to convert in Fahrenheit: ")
 
-converted_ctemp = convert_to_celcius(ftemp)
-converted_ftemp = convert_to_fahrenheit(ctemp)
+ctof = convert_to_fahrenheit(ctemp)
+ftoc = convert_to_celcius(ftemp)
 
-print(ftemp, "in Celsius is", converted_ctemp)
-print(ctemp, "in Fah is", converted_ftemp)
+print(ctemp, "in Fahrenheit is", ctof)
+print(ftemp, "in Celsius is", ftoc)
