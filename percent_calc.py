00e3e7c1545b50501b1feebee20647b2f6a49a0b
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-05-12 17:46:10 -0700

    Update percent_calc.py

--- percent_calc.py
+++ percent_calc.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+#get percentage 1 num (x) is of other num (y)
+#
+
+def get_percent(y, z):
+    y = y*100
+    x = y/z
+    return(x)
+
+y = input("Enter first number:")
+z = input("Enter second number:")
+
+percent = get_percent(y, z)
+print(percent)
