d739da6da00c21a70e974f85900cdfaa184e34b7
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-08-23 16:49:46 -0700

    Update convert_metric.py

--- convert_metric.py
+++ convert_metric.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def convert_lb_to_kg(weight_in_lb):
-    weight_in_kg = weigh_in_lb * .45359237
+    weight_in_kg = weight_in_lb * .45359237
     return weight_in_kg
     
 def convert_kg_to_lb(weight_in_kg):
@@ -12,7 +12,7 @@ try:
     weight_in_lb = float(input("Enter weight in lbs(0 if unknown):"))
     weight_in_kg = float(input("Enter weight in kg(0 if unknown):"))
     converted_weight_kg = convert_lb_to_kg(weight_in_lb)
-    converted_weight_lb = cconvert_kg_to_lb(weight_in_kg)
+    converted_weight_lb = convert_kg_to_lb(weight_in_kg)
     print(weight_in_lb, "lb(s) =", round(converted_weight_kg, 2), "in kg")
     print(weight_in_kg, "kg(s) =", round(converted_weight_lb, 2), "in lb")
 except:
