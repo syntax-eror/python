320927b7a13599b3e664875e5939f89faed9ff1e
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-11-22 12:53:17 -0700

    Create zlib_decompress.py

---
+++ zlib_decompress.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#simple script to decompress zlib files
+#stolen from Jason Wright SANS instructor sorry Jason
+
+import sys
+from zlib import *
+
+indata=open(sys.argv[1], "rb").read()
+outdata=decompress(indata)
+with open(sys.argv[1] + ".out", "wb") as f:
+    f.write(outdata)
+    f.close()
