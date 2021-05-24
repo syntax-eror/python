c2c2a2792f404c4f5245c18dd7ced63cbf995223
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2021-05-24 02:08:22 +0000

    Update excel_openpyxl.py

--- excel_openpyxl.py
+++ excel_openpyxl.py
@@ -2,6 +2,11 @@
 
 import openpyxl #pip3 install openpyxl
 
+try:
+    location = input("Enter location of file:")
+except:
+    print("Invalid location double check")
+
 wb = openpyxl.load_workbook('/mnt/c/temp/test.xlsx') #replace with var
 sheet = wb['Sheet1'] #can call specific sheets in a wb; Sheet1 is the default
 cella1 = sheet['A1']
