205d1e632781b343ad52103540d2e712cef3a9e1
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2021-05-24 02:57:45 +0000

    Update excel_openpyxl.py

--- excel_openpyxl.py
+++ excel_openpyxl.py
@@ -13,3 +13,12 @@ cella1 = sheet['A1']
 print(cella1.value)
 sheet['A1'] = 1 #assign value directly to cell
 sheet['A2'] = None #set value to None
+
+for i in range(1, 8):
+    print(i, sheet.cell(row=i, column=1).value)
+    
+for i in range(1, 8):
+    if sheet.cell(row=i, column=1).value == 172.16.39.1:
+        sheet.cell(row=i, column=1).value = None
+    else:
+        print(i, sheet.cell(row=i, column=1).value)
