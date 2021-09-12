63a23a787e657c1f80fe3514b81f6a736b1850a6
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2021-09-12 19:19:00 +0000

    Update excel_remove_ip_addresses.py

--- excel_remove_ip_addresses.py
+++ excel_remove_ip_addresses.py
@@ -2,14 +2,19 @@
 
 #current issues:
 #if sheet has duplicates of multiple IPs, it only removes one
-#range has to be set manually; could a while loop be used instead
+#range has to be set manually; could a while loop be used instead?
+
+#to do:
+#remove duplicates?
+#set up way to read from xlsx file instead of txt file
 #need to break everything into functions
 
 import openpyxl #module for working with excel files
 
 wb = openpyxl.load_workbook('/mnt/c/temp/test.xlsx') #load the workbook with IPs to clean up
-sheet = wb['Sheet1'] #set the active sheet; only works with default single sheet for now
-ipremove_list = input("Enter exact path of list of IPs to remove: ") #location of list of IPs that should be removed from sheet
+sheet = wb['Sheet1'] #set the active sheet; only works with default single sheet Sheet1 for now
+ipremove_list = 'mnt/c/temp/ip.txt'
+#ipremove_list = input("Enter exact path of list of IPs to remove: ") #location of list of IPs that should be removed from sheet
 
 try:
     with open(ipremove_list) as ipremovelist_file:
@@ -24,14 +29,13 @@ except: #generic error catching
 
 wb.save('/mnt/c/temp/testupdate3.xlsx') #save result as new excel file
 
-
 #for i in range(1, 8):
     #print(i, sheet.cell(row=i, column=1).value)
 #    if sheet.cell(row=i, column=1).value == '10.10.0.3':
         #print("found")
         #sheet.cell(row=i, column=1).value = None
         #print("New value:", sheet.cell(row=i, column=1).value)
-        sheet.delete_rows(i)
+        #sheet.delete_rows(i)
 #    else:
         #print("Nothing found")
 #        continue
