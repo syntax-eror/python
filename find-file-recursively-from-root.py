0f5feb255c738c773eb584e0f9f7b3ba9d62606b
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2022-05-20 18:12:40 +0000

    Update find-file-recursively-from-root.py

--- find-file-recursively-from-root.py
+++ find-file-recursively-from-root.py
@@ -3,4 +3,8 @@ import os
 
 path = os.path.abspath(os.sep)
 
-for dirpath, dirs, files in os.walk(path)
+for dirpath, dirs, files in os.walk(path):
+    for filename in files:
+        fname = os.path.join(dirpath,filename)
+        if fname.endswith('.txt'):
+            print(fname)
